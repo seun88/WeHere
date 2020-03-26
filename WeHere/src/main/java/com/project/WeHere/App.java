6fb47c93801071657355d46fb7ff4fca11c70ff0
seun akinbode <seunakinbode@yahoo.com>
2020-03-26 11:14:02 +0000

    first commit

---
+++ WeHere/src/main/java/com/project/WeHere/App.java
@@ -0,0 +1,13 @@
+package com.project.WeHere;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
